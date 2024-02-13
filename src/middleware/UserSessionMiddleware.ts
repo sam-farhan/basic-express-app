3b496f247e21b8be6fd59e053e9978fa4b248d99
Sam Farhan <sam.farhan@finaps.nl>
2024-02-13 22:40:35 +0100

    Logic to check if a user does not have a session for certain routes.

--- src/middleware/UserSessionMiddleware.ts
+++ src/middleware/UserSessionMiddleware.ts
@@ -7,10 +7,22 @@ export function AddUserSessionToLocals (req: Request, res: Response, next: NextF
     next();
 };
 
+// Checks if the user has a session.
+// If they don't, take them to the sign in page.
 export function CheckUserSession(req: Request, res: Response, next: NextFunction) {
     const userSession = GetUserSession(req);
     if (!userSession) {
         return res.redirect("/auth/signin");
+    }
+    next();
+}
+
+// Checks if the user has no session.
+// If they do, take them to the profile page.
+export function CheckNoUserSession(req: Request, res: Response, next: NextFunction) {
+    const userSession = GetUserSession(req);
+    if (userSession) {
+        return res.redirect("/user/profile");
     }
     next();
 }
--- src/routes/AuthRoutes.ts
+++ src/routes/AuthRoutes.ts
@@ -2,37 +2,38 @@ import { Router, Request, Response } from 'express';
 import { body } from 'express-validator';
 import { userEmailIsUnique, userNameIsUnique, userPasswordIsValid } from '../validators/AuthValidation';
 import { SignIn, SignOut, SignUp } from '../controllers/AuthControllers';
+import { CheckNoUserSession, CheckUserSession } from '../middleware/UserSessionMiddleware';
 
 const router = Router();
 
 // Signup page.
-router.get('/signup', (req: Request, res: Response) => {
+router.get('/signup', CheckNoUserSession, (req: Request, res: Response) => {
     res.render("pages/auth/signup");
 });
 
 // Signup page.
-router.get('/signin', (req: Request, res: Response) => {
+router.get('/signin', CheckNoUserSession, (req: Request, res: Response) => {
     res.render("pages/auth/signin");
 });
 
 
 // Signup post.
-router.post("/signup", 
+router.post("/signup", CheckNoUserSession,
     body('email').isEmail().normalizeEmail().custom(userEmailIsUnique),
     body('username').not().isEmpty().trim().escape().custom(userNameIsUnique),
     body('password').custom(userPasswordIsValid),
     SignUp
 );
 
-// Signup in.
-router.post("/signin", 
+// Signin post.
+router.post("/signin", CheckNoUserSession,
     body('username').not().isEmpty().trim().escape(),
     body('password').custom(userPasswordIsValid),
     SignIn
 );
 
-// Sign out.
-router.post("/signout",
+// Signout post.
+router.post("/signout", CheckUserSession,
     SignOut
 );
 
