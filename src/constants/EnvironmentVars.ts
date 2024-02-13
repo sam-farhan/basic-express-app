abe724864e946eb1746cb95ba311abdd7e7bb8b2
Sam Farhan <sam.farhan@finaps.nl>
2024-02-13 22:35:42 +0100

    Typo in environment variable.

--- src/constants/EnvironmentVars.ts
+++ src/constants/EnvironmentVars.ts
@@ -9,6 +9,6 @@ export default {
         PORT: parseInt(process.env.DATABASE_PORT || "5432"),
         NAME: process.env.DATABASE_NAME || "app_db",
         USER: process.env.DATABASE_USER || "postgres",
-        PASSWORD: process.env.DATABaSE_PASSWORD || "Test123!",
+        PASSWORD: process.env.DATABASE_PASSWORD || "Test123!",
     }
 } as const;
